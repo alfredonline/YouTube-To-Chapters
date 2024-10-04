9abaa2a51d496c88337b78a0db0072b9183685e7
Alfie <afortd@yahoo.com>
2024-10-04 18:39:03 +0200

    update url

--- utils/stripe/index.ts
+++ utils/stripe/index.ts
@@ -52,8 +52,8 @@ export async function hasSubscription(): Promise<{
 
 export async function createCheckoutLink(customer: string) {
   const checkout = await stripe.checkout.sessions.create({
-    success_url: `http://localhost:3000/dashboard`,
-    cancel_url: `http://localhost:3000/dashboard`,
+    success_url: `https://www.youtubetochapters.com/dashboard`,
+    cancel_url: `https://www.youtubetochapters.com/dashboard`,
     customer: customer,
     line_items: [
       {
@@ -71,7 +71,7 @@ export async function generateCustomerPortalLink(customerId: string) {
   try {
     const portalSession = await stripe.billingPortal.sessions.create({
       customer: customerId,
-      return_url: "http://localhost:3000/dashboard",
+      return_url: "https://www.youtubetochapters.com/dashboard",
     });
 
     return portalSession.url;
