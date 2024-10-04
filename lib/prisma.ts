1fa7a0b87d4eab661c45d6becba583510804949b
Alfie <afortd@yahoo.com>
2024-10-04 14:28:58 +0200

    fix prisma file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+};
 
-// Remove the namespace declaration and use module augmentation
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  });
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
